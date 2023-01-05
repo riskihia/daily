a01e04f6a579184a75970111a452bc4b7209a885
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-01-05 21:38:24 +0700

    commit change color ngetik animation

--- kamis-5/script.js
+++ kamis-5/script.js
@@ -17,3 +17,30 @@ toggleClose.addEventListener('click', function(){
     sidebar.style.left = '-200px';
 });
   
+
+var text = 'Lorem ipsum dolor sit amet';
+var textArray = text.split('');
+var typingText = document.getElementById('typing-text');
+
+var colors = ["red", "blue", "green", "yellow"];
+// console.log(colors.random());
+
+var randomIndex = Math.floor(Math.random() * colors.length);
+var randomString = colors[randomIndex];
+var x = 0;
+
+function animateText() {
+    console.log(randomString);
+  if (textArray.length > 0) {
+    x++;
+    console.log(x);
+    randomIndex = Math.floor(Math.random() * colors.length);
+    randomString = colors[randomIndex];
+    typingText.innerHTML += textArray.shift();
+    typingText.style.color = randomString;
+    console.log(randomString);
+    setTimeout(animateText, 500);
+  }
+}
+
+animateText();
